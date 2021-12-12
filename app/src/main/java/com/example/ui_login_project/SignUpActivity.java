50285850ea26a9453ce0d62800b8eb9dc0c0955d
Meang Sung Joo <meangsungjoo@naver.com>
2021-12-12 21:12:07 +0900

    change email pass

--- app/src/main/java/com/example/ui_login_project/SignUpActivity.java
+++ app/src/main/java/com/example/ui_login_project/SignUpActivity.java
@@ -73,9 +73,8 @@ public class SignUpActivity extends AppCompatActivity
         }
 
 
-
+//
         Call<MY_USER_Data> call = MyRetrofitClient.getRetrofit().create(My_api.class).createNewAcount(name,email,pass);
-
 
         call.enqueue(new Callback<MY_USER_Data>() {
 
